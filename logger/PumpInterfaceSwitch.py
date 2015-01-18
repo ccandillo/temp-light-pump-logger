d8a5a5056748b69e727a448cda173f1098f26ab1
Chris <ccandillo@gmail.com>
2015-01-17 18:53:55 -0600

    now catches control-c to exit gracefully

--- logger/PumpInterfaceSwitch.py
+++ logger/PumpInterfaceSwitch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-'''Press the button to turn on the pump.  Release to turn off.'''
+'''Allows the pump to be controled by the onboard pushbutton switch'''
+import sys
 import time
 import RPi.GPIO as GPIO
 from PumpInterface import Pump
@@ -9,6 +10,13 @@ import logging
 import logging.config
 logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
 logger = logging.getLogger('PumpInterfaceSwitch')
+
+# configure gpio pins
+pushbutton_pin = 25
+
+# configure gpio
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pushbutton_pin, GPIO.IN)
 
 
 class PumpSwitch(Pump):
@@ -33,22 +41,26 @@ class PumpSwitch(Pump):
         self.stop()
 
 
-GPIO.setmode(GPIO.BCM)
-pushbutton_pin = 25
-GPIO.setup(pushbutton_pin, GPIO.IN)
+def run():
+    '''Press the button to turn on the pump.  Release to turn off.'''
 
-pump = PumpSwitch()
+    pump = PumpSwitch()
 
-previous_input = 0
-while True:
-    GPIO.setup(pushbutton_pin, GPIO.IN)
-    pin_input = GPIO.input(pushbutton_pin)
-    if ((previous_input == 0) and (pin_input == 1)):
-        pump.start()
-        previous_input = 1
+    previous_input = 0
+    while True:
+        GPIO.setup(pushbutton_pin, GPIO.IN)
+        pin_input = GPIO.input(pushbutton_pin)
+        if ((previous_input == 0) and (pin_input == 1)):
+            pump.start()
+            previous_input = 1
 
-    if ((previous_input == 1) and (pin_input == 0)):
-        pump.stop()
-        previous_input = 0
+        if ((previous_input == 1) and (pin_input == 0)):
+            pump.stop()
+            previous_input = 0
 
 
+if __name__ == '__main__':
+    try:
+        run()
+    except KeyboardInterrupt:
+        sys.exit(0)
